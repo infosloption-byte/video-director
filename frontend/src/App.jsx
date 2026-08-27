6e7f132ff647bf59d405618d1e9aa874439670b0
Yaddehi Chathura Umayanga Sriyarathna <info.sloption@gmail.com>
2026-08-27 21:08:56 +0530

    Add research route to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import SignalsPage from "./pages/SignalsPage";
+import ResearchPage from "./pages/ResearchPage";
 import StoryboardPage from "./pages/StoryboardPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<SignalsPage />} />
+      <Route path="/research/:id" element={<ResearchPage />} />
       <Route path="/storyboard/:id" element={<StoryboardPage />} />
     </Routes>
   );
