065be2d90431f7a2cd229abe9751b9bc39bd10c9
Yaddehi Chathura Umayanga Sriyarathna <info.sloption@gmail.com>
2026-08-28 14:44:40 +0530

    feat(auth): add recovery and email verification routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import SignalsPage from "./pages/SignalsPage";
 import ResearchPage from "./pages/ResearchPage";
 import StoryboardPage from "./pages/StoryboardPage";
 import MyResearchPage from "./pages/MyResearchPage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import VerifyEmailPage from "./pages/VerifyEmailPage";
 import { AuthProvider, authRequired, useAuth } from "./context/AuthContext";
 
 function ProtectedRoute({ children }) {
@@ -23,6 +26,9 @@ export default function App() {
         <Route path="/" element={<SignalsPage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/verify-email" element={<VerifyEmailPage />} />
         <Route path="/my-research" element={<ProtectedRoute><MyResearchPage /></ProtectedRoute>} />
         <Route path="/research/:id" element={<ProtectedRoute><ResearchPage /></ProtectedRoute>} />
         <Route path="/storyboard/:id" element={<ProtectedRoute><StoryboardPage /></ProtectedRoute>} />
