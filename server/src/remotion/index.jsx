0c2ce40d2409fc3387cc17ec25755f91b5e2eb31
Yaddehi Chathura Umayanga Sriyarathna <info.sloption@gmail.com>
2026-08-29 12:34:47 +0530

    feat: register canonical editor Remotion composition

--- server/src/remotion/index.jsx
+++ server/src/remotion/index.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import { Composition, registerRoot } from "remotion";
 import { HelixComposition, compositionMetadata } from "./Composition.jsx";
+import { HelixEditorComposition, editorCompositionMetadata } from "./EditorComposition.jsx";
 
 export function RemotionRoot() {
   return (
-    <Composition
-      id="HelixReel"
-      component={HelixComposition}
-      durationInFrames={1}
-      fps={30}
-      width={1080}
-      height={1920}
-      defaultProps={{ scenes: [] }}
-      calculateMetadata={compositionMetadata}
-    />
+    <>
+      <Composition
+        id="HelixReel"
+        component={HelixComposition}
+        durationInFrames={1}
+        fps={30}
+        width={1080}
+        height={1920}
+        defaultProps={{ scenes: [] }}
+        calculateMetadata={compositionMetadata}
+      />
+      <Composition
+        id="HelixEditorReel"
+        component={HelixEditorComposition}
+        durationInFrames={1}
+        fps={30}
+        width={1080}
+        height={1920}
+        defaultProps={{ timeline: { fps: 30, width: 1080, height: 1920, duration: 0, tracks: [] } }}
+        calculateMetadata={editorCompositionMetadata}
+      />
+    </>
   );
 }
 
