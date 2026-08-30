64cf267e07a8076f7754c05951dd34a898f0b9c6
Yaddehi Chathura Umayanga Sriyarathna <info.sloption@gmail.com>
2026-08-30 12:50:47 +0530

    feat: add AI editor assistant route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import SignalsPage from "./pages/SignalsPage";
 import ResearchPage from "./pages/ResearchPage";
 import StoryboardPage from "./pages/StoryboardPage";
 import AdvancedEditorPage from "./pages/AdvancedEditorPage";
+import EditorAIAssistantPage from "./pages/EditorAIAssistantPage";
 import EditorRenderPage from "./pages/EditorRenderPage";
 import MediaLibraryPage from "./pages/MediaLibraryPage";
 import EditorMediaPickerPage from "./pages/EditorMediaPickerPage";
@@ -39,6 +40,7 @@ export default function App() {
         <Route path="/research/:id" element={<ProtectedRoute><ResearchPage /></ProtectedRoute>} />
         <Route path="/storyboard/:id" element={<ProtectedRoute><StoryboardPage /></ProtectedRoute>} />
         <Route path="/editor/:id" element={<ProtectedRoute><AdvancedEditorPage /></ProtectedRoute>} />
+        <Route path="/editor/:id/ai" element={<ProtectedRoute><EditorAIAssistantPage /></ProtectedRoute>} />
         <Route path="/editor/:id/render" element={<ProtectedRoute><EditorRenderPage /></ProtectedRoute>} />
         <Route path="/editor/:id/media-picker" element={<ProtectedRoute><EditorMediaPickerPage /></ProtectedRoute>} />
         <Route path="/media/:id" element={<ProtectedRoute><MediaLibraryPage /></ProtectedRoute>} />
