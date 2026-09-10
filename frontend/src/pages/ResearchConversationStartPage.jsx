a43c23e02a58aae493e32722b46f5ac88f512023
Yaddehi Chathura Umayanga Sriyarathna <info.sloption@gmail.com>
2026-09-10 12:11:52 +0530

    Fix useProjects imports after context refactor

--- frontend/src/pages/ResearchConversationStartPage.jsx
+++ frontend/src/pages/ResearchConversationStartPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
-import { useProjects } from "../context/ProjectContext.jsx";
+import { useProjects } from "../context/useProjects.js";
 import "./ResearchConversationPage.css";
 
 export default function ResearchConversationStartPage() {
